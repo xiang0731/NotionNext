792315aa041e4be174cf2a5c06c069a14e959f82
Steve17 <xiang0731@vip.qq.com>
2025-10-11 14:06:53 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://wow17.com' }, // 17改动，复制粘贴。
+    { title: '重建并迁移中，敬请期待', url: 'https://wow17.com' } // 17改动，复制粘贴。
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,12 +18,13 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '分享方法', // 17改动，复制粘贴。
+  HEO_HERO_TITLE_2: '与思维认知', // 17改动，复制粘贴。
+  HEO_HERO_TITLE_3: 'WOW17.com', // 17改动，复制粘贴。
+  HEO_HERO_TITLE_4: '新版上线', // 17改动，复制粘贴。
+  HEO_HERO_TITLE_5: '持续更新', // 17改动，复制粘贴。
+  HEO_HERO_TITLE_LINK: 'https://wow17.com', // 17改动，复制粘贴。
+
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
@@ -49,12 +50,12 @@ const CONFIG = {
   ],
 
   // 个人资料底部按钮
-  HEO_INFO_CARD_URL1: '/about',
-  HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
-  HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+  HEO_INFO_CARD_URL1: '/about', // 17改动，复制粘贴。
+  HEO_INFO_CARD_ICON1: 'fas fa-user', // 17改动，复制粘贴。
+  HEO_INFO_CARD_URL2: 'https://github.com/xiang0731', // 17改动，复制粘贴。
+  HEO_INFO_CARD_ICON2: 'fab fa-github', // 17改动，复制粘贴。
+  HEO_INFO_CARD_URL3: 'https://www.wow17.com', // 17改动，复制粘贴。
+  HEO_INFO_CARD_TEXT3: '了解更多', // 17改动，复制粘贴。
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
@@ -117,10 +118,11 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: '留言板', // 17改动，复制粘贴。
+  HEO_SOCIAL_CARD_TITLE_2: '欢迎留下你的意见', // 17改动，复制粘贴。
+  HEO_SOCIAL_CARD_TITLE_3: '点击留言', // 17改动，复制粘贴。
+  HEO_SOCIAL_CARD_URL: 'https://wow17.com/advise', // 17改动，复制粘贴。
+
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
